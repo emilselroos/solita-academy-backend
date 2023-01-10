124f6d4b095beee4a57814d091f477eb70db4479
Emil Selroos <emil.selroos@gmail.com>
2023-01-10 23:36:19 +0200

    Added migration for journeys data

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import _ from 'lodash';
 import csv from 'csv-parser';
 import connection from './connection.js';
 
@@ -31,6 +32,9 @@ type JourneyRecord = {
 	duration: number, // in seconds
 };
 
+/*
+ * fetch all stations from csv
+*/
 const fetchStations = async () => {
 
 	const headers: Array<string> = [ 'fid', 'id', 'nimi', 'namn', 'name', 'osoite', 'address', 'kaupunki', 'stad', 'operaattori', 'kapasiteetti', 'x', 'y' ];
@@ -77,19 +81,85 @@ const fetchStations = async () => {
 
 				});
 			})
-			.on('error', (error: any) => (console.error(error), reject('error')));
+			.on('error', (error: any) => (console.error(error), reject(error)));
 	});
 
 	console.log(`[Migration] Migration of stations done!`);
 }
 
-// const fetchJourneys = () => {}
+/*
+ * fetch all journeys from csv
+*/
+const fetchJourneys = async () => {
+
+	const headers: Array<string> = [ 'departure_time', 'return_time', 'departure_station_id', 'departure_station_name', 'return_station_id', 'return_station_name', 'distance', 'duration' ];
+	const records: JourneyRecord[] = [];
+	const streams = [
+		'src/data/journeys_2021_05.csv',
+		'src/data/journeys_2021_06.csv',
+		'src/data/journeys_2021_07.csv'
+	];
+
+	console.log(`[Migration] Migrating journeys data...`);
+	
+	const handleJourneysFile = (file: string) => {
+		console.log(`[Migration] Starting to process file ${file}`);
+		return new Promise((resolve, reject) => {
+
+			fs.createReadStream(file)
+				.pipe(csv({
+					skipLines: 1,
+					headers: headers,
+				}))
+				.on('data', async (data: JourneyRecord) => {
+					// Skip journeys less than 10 seconds and / or less than 10 meters
+					if (data.distance > 9 && data.duration > 9) {
+						// Skip records which have missing / incorrect station relations
+						// @TODO: We should ask complete list from the client or insert following stations manually
+						if (data.departure_station_id != 997 && data.return_station_id != 997
+							&& data.departure_station_id != 999 && data.return_station_id != 999
+							&& data.departure_station_id != 754 && data.return_station_id != 754) {
+							records.push(data);
+						}
+					}
+				})
+				.on('end', async () => {
+					try {
+						// Due to large size of our datasets, we faced some memory issues
+						// Let's use chunks to insert data in smaller batches (apparently Sequelize ORM
+						// doesn't support that function anymore - that's why lodash)
+						const chunks = _.chunk(records, 1000);
+						for (const chunk of chunks) {
+							await Journey?.bulkCreate(chunk);
+						}
+						console.log(`[Migration] ${records.length} journeys found and added.`);
+						resolve(true);
+					} catch (error) {
+						console.error(`[Migration] Error while migrating journeys: `, error);
+					}
+				})
+				.on('error', (error: any) => (console.error(error), reject(error)));
+
+		});
+	}
+
+	// Go through 3 given journey datasets
+	try {
+		for (const file of streams) {
+			await handleJourneysFile(file);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+	console.log(`[Migration] Migration of journeys done!`);
+}
 
 const migrateData = async () => {
 	try {
 		await fetchStations();
-		// await fetchJourneys();
-		console.log(`[Migration] Migration done!`);
+		await fetchJourneys();
+		console.log(`[Migration] Done!`);
 		process.exit(1);
 	} catch (error) {
 		console.error(`[Migration] Error when migrating datasets: `, error);
