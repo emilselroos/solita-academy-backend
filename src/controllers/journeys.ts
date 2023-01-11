5cc268463f2620fbf6aa4826fa9845985c01a597
Emil Selroos <emil.selroos@gmail.com>
2023-01-11 21:25:38 +0200

    Added support for station and journey creation through API endpoint

--- src/controllers/journeys.ts
+++ src/controllers/journeys.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import connection from '../database/connection.js';
 const { Journey } = connection.models;
 
-interface Journey {
+interface IJourney {
 	JID: number,
 	departure_time: number,
 	return_time: number,
@@ -12,30 +12,71 @@ interface Journey {
 	duration: number
 }
 
-/* 
- * all journeys
+/*
+ * get all journeys
 */
-const getJourneys = async (request: Request, response: Response, next: NextFunction) => {
-	// @TODO: replace hard limit with backend pagination
-	let journeys: Journey[] = await Journey?.findAll({ limit: 500 });
-	return response.status(200).json({
-		data: journeys,
-	});
+const getJourneys = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		// @TODO: replace hard limit with backend pagination
+		let journeys: IJourney[] = await Journey?.findAll({ limit: 500 });
+		return res.status(200).json({
+			data: journeys,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
 }
 
-/* 
- * single journey
+/*
+ * get single journey
 */
-const getJourney = async (request: Request, response: Response, next: NextFunction) => {
-	let id: String = request.params.id;
-	let journey: Journey = await Journey?.findOne({
-		where: {
-			JID: id,
-		}
-	});
-	return response.status(200).json({
-		data: journey,
-	});
+const getJourney = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let id: string = req.params.id;
+		let journey: IJourney = await Journey?.findOne({
+			where: {
+				JID: id,
+			}
+		});
+		return res.status(200).json({
+			data: journey,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
 }
 
-export default { getJourneys, getJourney };
+/*
+ * create a new journey
+*/
+const createJourney = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let data = req.body;
+		let newJourney: IJourney = await Journey?.create(data);
+		res.status(200).json({
+			data: newJourney,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
+}
+
+export default { getJourneys, getJourney, createJourney };
--- src/controllers/stations.ts
+++ src/controllers/stations.ts
@@ -2,39 +2,81 @@ import { Request, Response, NextFunction } from 'express';
 import connection from '../database/connection.js';
 const { Station } = connection.models;
 
-interface Station {
-	SID: Number,
-	name: String,
-	address: String,
-	city: String,
-	capasity: Number,
-	x: Number,
-	y: Number
+interface IStation {
+	SID: number,
+	station_number: number,
+	name: string,
+	address: string,
+	city: string,
+	capasity: number,
+	x: number,
+	y: number
 }
 
-/* 
- * all stations
+/*
+ * get all stations
 */
-const getStations = async (request: Request, response: Response, next: NextFunction) => {
-	let stations: Station[] = await Station?.findAll();
-	return response.status(200).json({
-		data: stations,
-	});
+const getStations = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let stations: IStation[] | []= await Station?.findAll() || [];
+		return res.status(200).json({
+			data: stations,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
 }
 
-/* 
- * single station
+/*
+ * get single station
 */
-const getStation = async (request: Request, response: Response, next: NextFunction) => {
-	let id: string = request.params.id || '';
-	let station: Station = await Station?.findOne({
-		where: {
-			SID: id,
-		}
-	});
-	return response.status(200).json({
-		data: station,
-	});
+const getStation = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let id: string = req.params.id || '';
+		let station: IStation = await Station?.findOne({
+			where: {
+				SID: id,
+			}
+		});
+		return res.status(200).json({
+			data: station,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
 }
 
-export default { getStations, getStation };
+/*
+ * create a new station
+*/
+const createStation = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		let data = req.body;
+		let newStation: IStation = await Station?.create(data);
+		res.status(200).json({
+			data: newStation,
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({
+			data: null,
+			error: {
+				message: "Something went wrong."
+			}
+		});
+	}
+}
+
+export default { getStations, getStation, createStation };
